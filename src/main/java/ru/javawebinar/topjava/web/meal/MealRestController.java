cee6c59c779e36117a27ede0afca1fb6d5f2c55a
Vlad Baharev <fuentes.vlad@gmail.com>
2018-02-13 16:54:34 +0300

    added meal controller

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,32 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.AuthorizedUser;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 
-public class MealRestController {
-    private MealService service;
+import java.util.Collection;
 
+@Controller
+public class MealRestController {
+
+    @Autowired
+    MealService service;
+
+    public Meal save(Meal meal) {
+        return service.save(meal, AuthorizedUser.id());
+    }
+
+    public void delete(int id) {
+        service.delete(id, AuthorizedUser.id());
+    }
+
+    public Meal get(int id) {
+        return service.get(id,AuthorizedUser.id() );
+    }
+
+    public Collection<Meal> getAll() {
+        return service.getAll(AuthorizedUser.id());
+    }
 }
