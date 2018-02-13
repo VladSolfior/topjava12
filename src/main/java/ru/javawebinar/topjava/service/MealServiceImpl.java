42eb8d2b3e87780178224eec5420c543c10e2c9d
Vlad Baharev <fuentes.vlad@gmail.com>
2018-02-13 16:01:53 +0300

    meal service layer

--- src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -1,9 +1,34 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.util.Collection;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
 public class MealServiceImpl implements MealService {
 
     private MealRepository repository;
 
+    @Override
+    public Meal save(Meal meal, int userId) {
+        return repository.save(meal, userId);
+    }
+
+    @Override
+    public void delete(int id, int userId) throws NotFoundException{
+        checkNotFoundWithId(repository.delete(id, userId), id);
+    }
+
+    @Override
+    public Meal get(int id, int userId) throws NotFoundException {
+        return checkNotFoundWithId(repository.get(id, userId),id);
+    }
+
+    @Override
+    public Collection<Meal> getAll(int userId) {
+        return repository.getAll(userId);
+    }
 }
