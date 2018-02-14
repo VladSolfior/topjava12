266e779984c04a683cc236b057a8a72886a7ad30
Vlad Baharev <fuentes.vlad@gmail.com>
2018-02-14 16:07:10 +0300

    updated rest controller

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -5,28 +5,60 @@ import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.AuthorizedUser;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
 
-import java.util.Collection;
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static ru.javawebinar.topjava.util.DateTimeUtil.*;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
 @Controller
 public class MealRestController {
 
     @Autowired
-    MealService service;
+    MealService mealService;
 
     public Meal save(Meal meal) {
-        return service.save(meal, AuthorizedUser.id());
+        return mealService.save(meal, AuthorizedUser.id());
     }
 
     public void delete(int id) {
-        service.delete(id, AuthorizedUser.id());
+        mealService.delete(id, AuthorizedUser.id());
     }
 
     public Meal get(int id) {
-        return service.get(id,AuthorizedUser.id() );
+        return mealService.get(id, AuthorizedUser.id());
     }
 
-    public Collection<Meal> getAll() {
-        return service.getAll(AuthorizedUser.id());
+    public Meal update(Meal meal) {
+        checkNew(meal);
+        checkNotFoundWithId(mealService.get(meal.getId(), AuthorizedUser.id()), meal.getId());
+        return mealService.save(meal, AuthorizedUser.id());
+    }
+
+    public List<MealWithExceed> getAll(HttpServletRequest request) {
+        LocalDate startDate = parseLocalDate(request.getParameter("startDate"), request);
+        LocalDate endDate = parseLocalDate(request.getParameter("endDate"), request);
+        LocalTime startTime = parseLocalTime(request.getParameter("startTime"), request);
+        LocalTime endTime = parseLocalTime(request.getParameter("endTime"), request);
+        return MealsUtil.getWithExceeded(
+                mealService.getAll(AuthorizedUser.id())
+                        .stream()
+                        .filter(meal -> isBetween(meal.getTime(),
+                                Objects.isNull(startTime) ? LocalTime.MIN : startTime,
+                                Objects.isNull(endTime) ? LocalTime.MAX : endTime))
+                        .filter(meal -> isBetween(meal.getDate(), Objects.isNull(startDate) ? LocalDate.MIN : startDate,
+                                Objects.isNull(endDate) ? LocalDate.MAX : endDate
+                        ))
+                        .collect(Collectors.toList())
+                , MealsUtil.DEFAULT_CALORIES_PER_DAY
+        );
     }
 }
